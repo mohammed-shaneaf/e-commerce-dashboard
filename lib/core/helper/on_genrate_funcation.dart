dbcad6a28f3e670afe7c34e83b8895b5d4ae102d
mohammed-shaneaf <mohammed.shaneaf.963@gmail.com>
2025-05-01 21:52:54 +0300

    first commit

---
+++ lib/core/helper/on_genrate_funcation.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:fruits_hub_dashboard/features/dashboard/views/dashboard_view.dart';
+
+Route<dynamic> onGenerateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case DashboardView.routeName:
+      return MaterialPageRoute(builder: (context) => const DashboardView());
+    default:
+      return MaterialPageRoute(
+        builder:
+            (context) =>
+                const Scaffold(body: Center(child: Text("Page Not Found"))),
+      );
+  }
+}
