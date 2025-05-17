920693a99fc6db9d55eacd7c2260aec90366b88e
mohammed-shaneaf <mohammed.shaneaf.963@gmail.com>
2025-05-17 13:11:42 +0300

    implement Upload File On FireBaseStoreageService

--- lib/core/services/fire_storage.dart
+++ lib/core/services/fire_storage.dart
@@ -1,10 +1,17 @@
+import 'dart:io';
+
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:fruits_hub_dashboard/core/services/stoarage_service.dart';
+import 'package:path/path.dart' as b;
 
 class FireStorage implements StoarageService {
-  final stoarageReference = FirebaseStorage.instance;
+  final stoarageReference = FirebaseStorage.instance.ref();
   @override
-  Future<String> uploadFile(String file) {
-    throw UnimplementedError();
+  Future<String> uploadFile(File file, String path) async {
+    String fileName = b.basename(file.path);
+    String extensionName = b.extension(file.path);
+    var fileReferance = stoarageReference.child('$path/$fileName.$extensionName');
+    await fileReferance.putFile(file);
+    return fileReferance.getDownloadURL();
   }
 }
